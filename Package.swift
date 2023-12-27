a7d39653e6599817ec8b5f392f46393e802e4bf7
Mario Casola <m.casola@uqu.do>
2023-12-28 00:44:34 +0700

    Replaced version 2.7.5 with a new binary

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "UqudoSDK",
             url: "https://rm.dev.uqudo.io/repository/uqudo-public/io/uqudo/sdk/iOS-Uqudo/2.7.5/iOS-Uqudo-2.7.5.zip",
-            checksum: "f71f0987ca1ca253ef1c0ae0e4ec836bfb1b46bc467ef7ab9fb7210346517996"
+            checksum: "7abb718257ee2aea9541b90254d642bc4e15d17bd3b96ac9b23ad8b165047453"
         )
     ]
 )
